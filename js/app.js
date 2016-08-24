f2ae46d69d8fe79448730ee4297aafb241df0361
delamorinierejh <delamorinierejh@gmail.com>
2016-08-24 10:32:04 +0100

    added prevent default window scrolling with arrow keys

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 
 var Tetris            = Tetris || {};
 
+
 window.addEventListener("keydown", function(e) {
     // space and arrow keys
     if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
     }
 }, false);
+
 
 Tetris.setup          = function(){
 
